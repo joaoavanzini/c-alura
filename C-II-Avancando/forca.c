6c88174c445523a5ae2225b451b417489f60b110
João Victor Palhares <falejoaovictor@hotmail.com>
2020-12-31 16:15:38 -0300

    percorrendo o array e descobrindo a posição
    percorre o array e descobre a posição que o usuário colocou a determinada letra

--- C-II-Avancando/forca.c
+++ C-II-Avancando/forca.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
 	char palavraSecreta[20];
@@ -10,6 +11,14 @@ int main(){
 	int enforcou = 0;
 
 	do{
+		char chute;
+		scanf("%c", &chute);
+
+		for(int i = 0; i < strlen(palavraSecreta); i++){
+			if(palavraSecreta[i] == chute){
+				printf("A posição %d tem essa letra\n", i);
+			}
+		}
 
 	}while(!acertou && !enforcou);
 }
