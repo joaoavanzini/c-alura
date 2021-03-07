380b2cc893cd407f0fa6ca57f0c6ff9b4036c2b2
João Victor Palhares <falejoaovictor@hotmail.com>
2021-03-06 21:35:24 -0300

    alocando matrizes dinamicamente

--- C-III-RecursosAvancados/fogefoge.c
+++ C-III-RecursosAvancados/fogefoge.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//matriz de 5 x 10 - o +1 no final é o \0
-char mapa[200][200+1];
+char** mapa;
 int linhas;
 int colunas;
+
+//malloc = alocação dinâmica
+//free = liberar a memória
 
 int main(){
 
@@ -18,22 +20,10 @@ int main(){
 	fscanf(f, "%d %d", &linhas, &colunas);
 	printf("linhas %d, colunas %d\n", linhas, colunas);
 
-	////////////////
-
-	int** v = malloc(sizeof(int*) * 5);
-	for(int i = 0; i < 5; i++){
-		v[i] = malloc(sizeof(int) * 10);
+	mapa = malloc(sizeof(char*) * linhas);
+	for(int i = 0; i < linhas; i++){
+		mapa[i] = malloc(sizeof(char) * (colunas+1));
 	}
-	v[0][0] = 10;
-	v[1][2] = 12;
-	printf("inteiro alocado %d, %d\n", v[0][0], v[1][2]);
-	
-	for(int i = 0; i < 5; i ++){
-		free(v[i]);
-	}
-	free(v);
-
-	////////////////
 
 	for(int i = 0; i < 5; i++){
 		fscanf(f, "%s", mapa[i]);
@@ -43,14 +33,10 @@ int main(){
 		printf("%s\n", mapa[i]);
 	}
 
-/*
-	for(int i = 0; i < 5; i++){
-		for(int j = 0;  j < 10; j++){
-			printf("%c", mapa[i][j]);
-		}
-		printf("\n");
-	}
-*/
-
 	fclose(f);
+
+	for(int i = 0; i < linhas; i++){
+		free(mapa[i]);
+	}
+	free(mapa);
 }
