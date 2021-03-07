36f7cd1c991f7a51c8e7c32a93598be36f0848d0
João Victor Palhares <falejoaovictor@hotmail.com>
2021-03-06 21:40:52 -0300

    separando o código em funções

--- C-III-RecursosAvancados/fogefoge.c
+++ C-III-RecursosAvancados/fogefoge.c
@@ -5,11 +5,21 @@ char** mapa;
 int linhas;
 int colunas;
 
-//malloc = alocação dinâmica
-//free = liberar a memória
+void liberaMapa(){
+	for(int i = 0; i < linhas; i++){
+		free(mapa[i]);
+	}
+	free(mapa);
+}
 
-int main(){
+void alocaMapa(){
+	mapa = malloc(sizeof(char*) * linhas);
+	for(int i = 0; i < linhas; i++){
+		mapa[i] = malloc(sizeof(char) * (colunas+1));
+	}
+}
 
+void leMapa(){
 	FILE* f;
 	f = fopen("mapa.txt", "r");
 	if(f == 0){
@@ -18,25 +28,23 @@ int main(){
 	}
 
 	fscanf(f, "%d %d", &linhas, &colunas);
-	printf("linhas %d, colunas %d\n", linhas, colunas);
 
-	mapa = malloc(sizeof(char*) * linhas);
-	for(int i = 0; i < linhas; i++){
-		mapa[i] = malloc(sizeof(char) * (colunas+1));
-	}
+	alocaMapa();
 
 	for(int i = 0; i < 5; i++){
 		fscanf(f, "%s", mapa[i]);
 	}
+	
+	fclose(f);
+}
+
+int main(){
+
+	leMapa();
 
 	for(int i = 0; i < 5; i++){
 		printf("%s\n", mapa[i]);
 	}
 
-	fclose(f);
-
-	for(int i = 0; i < linhas; i++){
-		free(mapa[i]);
-	}
-	free(mapa);
+	liberaMapa();
 }
---
+++ C-III-RecursosAvancados/fogefoge.h
@@ -0,0 +1,2 @@
+void liberaMapa();
+void leMapa();
