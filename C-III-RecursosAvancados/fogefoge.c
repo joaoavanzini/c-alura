37e33200b7046fd3a95bd86743315f038a0bb048
João Victor Palhares <falejoaovictor@hotmail.com>
2021-03-06 22:02:37 -0300

    definindo um struct

--- C-III-RecursosAvancados/fogefoge.c
+++ C-III-RecursosAvancados/fogefoge.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "fogefoge.h"
 
-char** mapa;
-int linhas;
-int colunas;
+
+struct mapa m;
 
 void liberaMapa(){
-	for(int i = 0; i < linhas; i++){
-		free(mapa[i]);
+	for(int i = 0; i < m.linhas; i++){
+		free(m.matriz[i]);
 	}
-	free(mapa);
+	free(m.matriz);
 }
 
 void alocaMapa(){
-	mapa = malloc(sizeof(char*) * linhas);
-	for(int i = 0; i < linhas; i++){
-		mapa[i] = malloc(sizeof(char) * (colunas+1));
+	m.matriz = malloc(sizeof(char*) * m.linhas);
+	for(int i = 0; i < m.linhas; i++){
+		m.matriz[i] = malloc(sizeof(char) * (m.colunas+1));
 	}
 }
 
@@ -27,12 +27,12 @@ void leMapa(){
 		exit(1);
 	}
 
-	fscanf(f, "%d %d", &linhas, &colunas);
+	fscanf(f, "%d %d", &(m.linhas), &(m.colunas));
 
 	alocaMapa();
 
 	for(int i = 0; i < 5; i++){
-		fscanf(f, "%s", mapa[i]);
+		fscanf(f, "%s", m.matriz[i]);
 	}
 	
 	fclose(f);
@@ -40,7 +40,7 @@ void leMapa(){
 
 void imprimeMapa(){
 	for(int i = 0; i < 5; i++){
-		printf("%s\n", mapa[i]);
+		printf("%s\n", m.matriz[i]);
 	}
 }
 
@@ -53,9 +53,9 @@ void move(char direcao){
 	int y;
 
 	// acha a posicao do foge foge
-	for(int i = 0; i < linhas; i++){
-		for(int j = 0; j < colunas; j++){
-			if(mapa[i][j] == '@'){
+	for(int i = 0; i < m.linhas; i++){
+		for(int j = 0; j < m.colunas; j++){
+			if(m.matriz[i][j] == '@'){
 				x = i;
 				y = j;
 				break;
@@ -65,20 +65,20 @@ void move(char direcao){
 
 	switch(direcao){
 		case 'a':
-			mapa[x][y-1] = '@';
+			m.matriz[x][y-1] = '@';
 			break;
 		case 'w':
-			mapa[x-1][y] = '@';
+			m.matriz[x-1][y] = '@';
 			break;
 		case 's':
-			mapa[x+1][y] = '@';
+			m.matriz[x+1][y] = '@';
 			break;
 		case 'd':
-			mapa[x][y+1] = '@';
+			m.matriz[x][y+1] = '@';
 			break;
 	}
 
-	mapa[x][y] = '.';
+	m.matriz[x][y] = '.';
 }
 
 int main(){
--- C-III-RecursosAvancados/fogefoge.h
+++ C-III-RecursosAvancados/fogefoge.h
@@ -1,2 +1,13 @@
 void liberaMapa();
 void leMapa();
+void alocaMapa();
+
+void move(char direcao);
+int acabou();
+void imprimeMapa();
+
+struct mapa{
+	char** matriz;
+	int linhas;
+	int colunas;
+};
