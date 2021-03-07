328854ad98b4a4e297c300184d27af5e73cd79c8
João Victor Palhares <falejoaovictor@hotmail.com>
2021-03-06 22:55:41 -0300

    ignorando comandos que não movem o herói

--- C-III-RecursosAvancados/fogefoge.c
+++ C-III-RecursosAvancados/fogefoge.c
@@ -12,6 +12,9 @@ int acabou(){
 
 void move(char direcao) {
 
+	if(direcao != 'a' && direcao != 'w' && direcao != 's' && direcao != 'd')
+		return;
+
     m.matriz[heroi.x][heroi.y] = '.';
 
     switch(direcao) {
